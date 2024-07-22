838258b65eef72708d592764d77c87583cb452a1
Suraj <surajreddy19012003@gmail.com>
2024-07-22 22:39:37 +0530

    first

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../model/user')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
 
